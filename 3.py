0b8e52ef70cabfac55de43362f4b5b3496e9c463
chikrizov.ki0029 <kostya1234moi@gmail.com>
2024-02-06 16:08:12 +0500

    изменено название переменной

--- 3.py
+++ 3.py
@@ -2,13 +2,13 @@ import sys
 from Samples.geocoder import get_coordinates, get_ll_span
 from Samples.mapapi_PG import show_map
 
-t_t_f = ' '.join(sys.argv[1:])
-if t_t_f:
-    lat, lon = get_coordinates(t_t_f)
+toponym_to_find = ' '.join(sys.argv[1:])
+if toponym_to_find:
+    lat, lon = get_coordinates(toponym_to_find)
     ll_spn = f'll={lat},{lon}&spn=0.005,0.005'
     show_map(ll_spn, 'map')
 
-    ll, spn = get_ll_span(t_t_f)
+    ll, spn = get_ll_span(toponym_to_find)
     ll_spn = f'll={ll}&spn={spn}'
     show_map(ll_spn, 'map')
 
